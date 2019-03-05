a3208505ba7c13bf426a9e1b80e71d4f6e6eba8a
Haoyuan Lin <specialemon@gmail.com>
2019-03-05 15:04:26 -0500

    mlab

--- seeds/seed-db.js
+++ seeds/seed-db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://specialemon:<PASSWORD>@cluster0-shard-00-00-qskyf.mongodb.net:27017,cluster0-shard-00-01-qskyf.mongodb.net:27017,cluster0-shard-00-02-qskyf.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true", { useNewUrlParser: true, useMongoClient: true }).then(function(){
-  console.log("connected to db");
+mongoose.connect("mongodb://specialemon:199199@cluster0-shard-00-00-qskyf.mongodb.net:27017,cluster0-shard-00-01-qskyf.mongodb.net:27017,cluster0-shard-00-02-qskyf.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true", { useNewUrlParser: true, useMongoClient: true }).then(function () {
+    console.log("connected to db");
 });
 
 const User = require('../models/User');
@@ -21,13 +21,13 @@ User.create([{
 },
 {
     Name: "Wesker"
-}])
-    .then(Users => {
-        console.log(`${Users.length} users created`);
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-    .finally(() => {
-        mongoose.connection.close();
-    });
+}
+]).then(Users => {
+    console.log(`${Users.length} users created`);
+}).catch((err) => {
+    console.log(err);
+}).finally(() => {
+    mongoose.connection.close();
+});
+
+
